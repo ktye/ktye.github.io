33732fb0e7acff9839c2a0cd9a0d1cb590345551
U-EMEA\csd80117 <ktye@users.noreply.github.com>
2022-07-15 12:32:02 +0200

    apl?

---
+++ zoo/j90.js
@@ -0,0 +1,19 @@
+
+let O
+function ini(left,o){O=o
+ let p=document.createElement("iframe")
+ p.src="../j/j90/J1990a.htm"
+ p.style.width="100%";p.style.height="100%";
+ left.style.overflow="hidden";
+ left.appendChild(p)
+
+ 
+ O("nyi\n    ")
+}
+
+function evl(s){ 
+}
+
+let j90={ini:ini,evl:evl}
+
+export { j90 }
