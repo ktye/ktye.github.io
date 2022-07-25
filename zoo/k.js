90b7319ea1928c352b48e7718fd0e6004a3edb6c
U-EMEA\csd80117 <ktye@users.noreply.github.com>
2022-07-25 17:31:20 +0200

    canvas

--- zoo/k.js
+++ zoo/k.js
@@ -1,20 +1,36 @@
 import { K  } from '../k.js'
+import { D  } from '../draw.js'
 import { fs } from './fs.js'
 
 
 function U(){ return new   Uint32Array(K._.memory.buffer) }
 function su(u){return (u.length)?new TextDecoder("utf-8").decode(u):""}
 function ge(x){return document.getElementById(x)}
+function ce(x){return document.createElement(x)}
 
 var O
+var ctx
 
 function ktry(s){
  try     { let x=K._.repl(K.KC(s)); K.save() }
  catch(e){ console.log(e); K.restore() }
 }
+function display(){
+ let cnv=ce("canvas")
+ cnv.id="_cnv"
+ cnv.style.position="absolute"
+ cnv.style.left=0;
+ cnv.style.bottom="0vh"
+ cnv.style.zIndex=10;
+ ctx=cnv.getContext("2d")
+ return cnv
+}
 
 function ini(left,o){O=o
- fetch("../readme").then(r=>r.text()).then(r=>{let p=document.createElement("pre");p.textContent=r;left.appendChild(p)});
+ fetch("../readme").then(r=>r.text()).then(r=>{
+  let p=ce("pre");p.textContent=r;left.appendChild(p)
+  left.appendChild(display())
+ });
 
  var ext={
   init: function( ){O("ktye/k\n ")},
@@ -24,6 +40,7 @@ function ini(left,o){O=o
 
  fs.init(U,O)
 
+ Object.assign(ext, D)
  K.kinit(ext,"../k.wasm")
  
  tty.drop=function(name,u){
