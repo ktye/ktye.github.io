140fdfca9b70c5c0a3a921b0856cacff405e465f
U-EMEA\csd80117 <ktye@users.noreply.github.com>
2022-07-25 16:17:27 +0200

    drop files

---
+++ zoo/files.js
@@ -0,0 +1,48 @@
+
+function ge(x){return document.getElementById(x)}
+function ce(x){return document.createElement(x)}
+function us(s){return new TextEncoder("utf-8").encode(s)}
+function su(u){return (u.length)?new TextDecoder("utf-8").decode(u):""}
+
+
+
+function ini(left,o){
+ let tty=ge("tty")
+ tty.onkeypress=undefined
+ let currentfile=""
+ let editfile=function(e){if(currentfile.length)fs.writefile(currentfile,us(tty.value))}
+ tty.onchange=editfile
+
+ let t=ce("table")
+ let keys=Object.keys(fs.files)
+
+ for(let i=0;i<keys.length;i++){
+  let filename=keys[i]
+  let u=fs.files[filename]
+  let r=ce("tr")
+  
+  let name=ce("td")
+  name.textContent=filename
+  name.style.color="blue"
+  name.onclick=function(){tty.value=su(u);currentfile=filename}
+  r.appendChild(name)
+  
+  let bytes=ce("td")
+  bytes.textContent=String(u.length)
+  r.appendChild(bytes)
+  t.appendChild(r)
+  
+  let a=ce("a")
+  a.textContent="↓"
+  r.appendChild(a)
+  let b=new Blob([u],{type:"application/octet-stream"})
+  a.href=URL.createObjectURL(b)
+  a.download=filename
+ }
+ left.appendChild(t)
+}
+function evl(s){}
+
+
+let files = {ini:ini,evl:evl,src:""}
+export { files } 
--- zoo/k.js
+++ zoo/k.js
@@ -4,11 +4,14 @@ import { fs } from './fs.js'
 
 function U(){ return new   Uint32Array(K._.memory.buffer) }
 function su(u){return (u.length)?new TextDecoder("utf-8").decode(u):""}
+function ge(x){return document.getElementById(x)}
 
 var O
 
-
-
+function ktry(s){
+ try     { let x=K._.repl(K.KC(s)); K.save() }
+ catch(e){ console.log(e); K.restore() }
+}
 
 function ini(left,o){O=o
  fetch("../readme").then(r=>r.text()).then(r=>{let p=document.createElement("pre");p.textContent=r;left.appendChild(p)});
@@ -22,11 +25,17 @@ function ini(left,o){O=o
  fs.init(U,O)
 
  K.kinit(ext,"../k.wasm")
+ 
+ tty.drop=function(name,u){
+  if(name.endsWith(".k")){O("\\l "+name+"\n"); ktry(u); O(" ")}
+ }
+ 
 }
 
 function evl(s){
  O("\n")
- K._.repl(K.KC(s))
+ //K._.repl(K.KC(s))
+ ktry(s)
  O(" ")
 }
 
