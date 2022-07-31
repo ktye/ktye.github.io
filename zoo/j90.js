9cc9142ed6707299f211b5906afa148b7b0fef7f
U-ultrahorst\elmar <ktye@users.noreply.github.com>
2022-07-31 15:49:51 +0200

    j90 up and running

--- zoo/j90.js
+++ zoo/j90.js
@@ -1,6 +1,27 @@
 function ge(x){return document.getElementById(x)}
 function ce(x){return document.createElement(x)}
 
+var txt = "Roger Hui: <blockquote>The first release of the software was at APL90 in Copenhagen in August 1990, when J was made available as shareware to the Software Exchange. (You can still get this version from various archives.)</blockquote>It turned out to be not so simple.<p>Thank's to Martin Neitzel for the binary and @copy for the v86 emulator<p>You can select text in the APL\\? paper (e.g. from the examples at the end) then press enter to send it to the emulator"
+
+// send marked text in APL\? pre elements to emulator on enter key.  
+// initialization needs to wait for APL\? iframe to be ready.
+var emulator
+function jlink(d){
+ let s=d.getSelection().getRangeAt(0).toString()+"\n"
+ for(let i=0;i<s.length;i++)emulator.keyboard_adapter.simulate_char(s[i])
+}
+var iframe
+function ready(){let p=iframe
+ let d=p.contentDocument||p.contentWindow.document
+ console.log(d.readyState)
+ if(d.readyState=="complete"){
+  console.log("complete")
+  d.onkeyup=function(e){if(e.key=="Enter")jlink(d)}
+  return
+ } 
+ window.setTimeout(ready,100)
+}
+
 let O
 function ini(left,o){O=o
  let p=ce("iframe")
@@ -8,6 +29,8 @@ function ini(left,o){O=o
  p.style.width="100%";p.style.height="100%";
  left.style.overflow="hidden";
  left.appendChild(p)
+ iframe=p;ready()
+ 
 
  
  let sc=ce("div"); sc.classList.add("right")
@@ -19,28 +42,35 @@ function ini(left,o){O=o
  let cn=ce("canvas")
  cn.style.display="none"
  sd.appendChild(cn)
+ let an=document.createElement("div")
+ an.innerHTML=txt
+ an.style.fontFamily="serif"
+ sc.appendChild(an)
  ge("tty").parentNode.append(sc)
+ 
 
  let dos="../j/j90/dos/"
  fetch(dos+"libv86.js").then(r=>r.text()).then(r=>{
   eval(r)
-  var emulator = window.emulator = new V86Starter({
+  emulator = window.emulator = new V86Starter({
    wasm_path: dos+"v86.wasm",
    memory_size: 32 * 1024 * 1024,
    vga_memory_size: 2 * 1024 * 1024,
    screen_container: sc,
    bios: { url: dos+"seabios.bin" },
    vga_bios: { url: dos+"vgabios.bin" },
-   fda: { url: dos+"freedos722.img", size: 737280 },
+   fda: { url: dos+"jdos.img", size: 737280 },
    autostart: true,
   })
  })
+
  
- O("nyi\n    ")
+ O("where was my apl90 floppy again?\n")
 }
 
 function evl(s){ 
 }
+
 
 let j90={ini:ini,evl:evl}
 
