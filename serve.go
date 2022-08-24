8c5e47998cccfdca9bf1a789b88a2be9165f9794
U-EMEA\csd80117 <ktye@users.noreply.github.com>
2022-08-24 14:30:35 +0200

    serve: post

--- serve.go
+++ serve.go
@@ -1,11 +1,27 @@
 package main
 
 import (
-        "net/http"
 	"fmt"
+	"io"
+	"net/http"
+	"os"
 )
 
+var F http.Handler
+
 func main() {
-        http.Handle("/", http.FileServer(http.Dir(".")))
-        fmt.Println(http.ListenAndServe(":3000", nil))
+	F = http.FileServer(http.Dir("."))
+	//http.Handle("/", http.FileServer(http.Dir(".")))
+	http.HandleFunc("/", handle)
+	fmt.Println(http.ListenAndServe(":3000", nil))
+}
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		fmt.Println("POST:", r.URL.Path)
+		io.Copy(os.Stdout, r.Body)
+		os.Stdout.Write([]byte{10})
+	} else {
+		F.ServeHTTP(w, r)
+	}
 }
