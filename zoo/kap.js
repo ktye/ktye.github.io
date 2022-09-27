161cb377464590660c1bfad0f8097d095f66de5b
U-EMEA\csd80117 <ktye@users.noreply.github.com>
2022-09-27 09:44:55 +0200

    kap

---
+++ zoo/kap.js
@@ -0,0 +1,15 @@
+
+let O
+let w
+function ini(left,o){O=o
+ w=new Worker("./kap/compute-queue-worker.js")
+ w.onmessage=function(e){let r=JSON.parse(e.data); //console.log(r);
+  switch(r.type){
+  case"avail":           O("KAP\n      ");               break;
+  case"response":        O("\n"+r.result+ "\n      ");   break;
+  case"evalexception":   O("\n"+r.message+"\n      ");   break;
+  default:console.log("response type?",r)
+}}}
+function evl(s){w.postMessage(JSON.stringify({src:s}))}
+let kap={ini:ini,evl:evl,src:""}
+export { kap }
