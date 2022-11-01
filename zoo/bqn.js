d30d7d5e09e6027f8f03a9e93d9c895bbfa0c8ee
U-EMEA\csd80117 <ktye@users.noreply.github.com>
2022-11-01 15:29:17 +0100

    lazy load bqn

--- zoo/bqn.js
+++ zoo/bqn.js
@@ -1,8 +1,12 @@
-import { Bqn } from './bqn/bqn.js'
+//import { Bqn } from './bqn/bqn.js'
 
-let O
+let O,Bqn
 function ini(left,o){O=o
  fetch("./bqn/ref.txt").then(r=>r.text()).then(r=>{let p=document.createElement("pre");p.textContent=r;left.appendChild(p)});
+ 
+ fetch("./bqn/bqn.js").then(r=>r.text()).then(r=>{
+  Bqn = new Function(r+"\n return{str:str,compile:compile,run:run,fmt:fmt}")()
+ })
  
  O("BQN\n  ")
 }
