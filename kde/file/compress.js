55b7355224aa88a39493e1cf622ba79106e98619
U-ultrahorst\elmar <ktye@users.noreply.github.com>
2022-11-20 17:54:54 +0100

    compress

--- kde/file/compress.js
+++ kde/file/compress.js
@@ -1,38 +1,30 @@
-/* from SnappyJs 0.7.0 Zhipeng Jia (MIT) https://github.com/zhipeng-jia/snappyjs */
-function compress(u){
- var gh=new Array(15)
- let hf=function(k,s){return(k*0x1e35a7bd)>>>s}
- let l32=function(a,p){return a[p]+(a[p+1]<<8)+(a[p+2]<<16)+(a[p+3]<<24)}
- let eq=function(a,p,q){return a[p]===a[q]&&a[p+1]===a[q+1]&&a[p+2]===a[q+2]&&a[p+3]===a[q+3]}
- let cp=function(x,a,y,b,n){for(let i=0;i<n;i++)y[b+i]=x[a+i]}
- let el=function(x,i,n,r,p){
-  if(n<=60){r[p]=(n-1)<<2;p+=1}else if(n<256){r[p]=60<<2;r[p+1]=n-1;p+=2}else{r[p]=61<<2;r[p+1]=(n-1)&0xff;r[p+2]=(n-1)>>>8;p+=3}
-  cp(x,i,r,p,n);return p+n}
- let ecl=function(r,p,o,n){
-  if(n<12&&o<2048){r[p]=1+((n-4)<<2)+((o>>>8)<<5);r[p+1]=o&0xff;return p+2}
-  else{r[p]=2+((n-1)<<2);r[p+1]=o&0xff;r[p+2]=o>>>8;return p+3}}
- let ec=function(r,p,o,n){while(n>=68){p=ecl(r,p,o,64);n-=64}
-  if(n>64){p=ecl(r,p,o,60);n-=60};return ecl(r,p,o,n)}
- let cf=function(x,p,xs,r,q){
-  var tb=1;while((1<<tb)<=xs&&tb<=14){tb+=1}
-  tb-=1;
-  var hfs=32-tb
-  if(typeof gh[tb]==='undefined'){gh[tb]=new Uint16Array(1<<tb)}
-  var ht=gh[tb]
-  for(let i=0;i<ht.length;i++)ht[i]=0
-  var pe=p+xs,l,bi=p,ne=p
-  var ha,nh,ni,c,sk,bb,ba,m,o,ph,ch,f=true,I=15
-  if(xs>=I){l=pe-I;p+=1;nh=hf(l32(x,p),hfs)
-   while(f){sk=32;ni=p
-    do{p=ni;ha=nh;bb=sk>>>5;sk+=1;ni=p+bb;if(p>l){f=false;break};nh=hf(l32(x,ni),hfs);c=bi+ht[ha];ht[ha]=p-bi}while(!eq(x,p,c))
-    if(!f){break};q=el(x,ne,p-ne,r,q)
-    do{
-     ba=p;m=4;while(p+m<pe&&x[p+m]===x[c+m]){m+=1};p+=m;o=ba-c;q=ec(r,q,o,m);ne=p;if(p>=l){f=false;break}
-     ph=hf(l32(x,p-1),hfs);ht[ph]=p-1-bi;ch=hf(l32(x,p),hfs);c=bi+ht[ch];ht[ch]=p-bi
-    }while(eq(x,p,c))
-    if(!f){break};p+=1;nh=hf(l32(x,p),hfs)
-  }};if(ne<pe)q=el(x,ne,pe-ne,r,q);return q}
- let pV=function(v,r,p){do{r[p]=v&0x7f;v=v>>>7;if(v>0)r[p]+=0x80;p+=1}while(v>0);return p}
- let maxlen=function(u){return 32+u.length+Math.floor(u.length/6)}
- let ctb=function(r,u){let l=u.length,p=0,n=0,s;n=pV(l,r,n);while(p<l){s=Math.min(l-p,1<<16);n=cf(u,p,s,r,n);p+=s};return n}
- let r=new Uint8Array(maxlen(u));let n=ctb(r,u);return r.slice(0,n)} 
+function compress(u){ //from SnappyJs 0.7.0 Zhipeng Jia (MIT) https://github.com/zhipeng-jia/snappyjs
+ let G=new Array(15)
+ let H=(k,s)=>(k*0x1e35a7bd)>>>s
+ let L=(a,p)=>a[p]+(a[p+1]<<8)+(a[p+2]<<16)+(a[p+3]<<24)
+ let E=(a,p,q)=>a[p]===a[q]&&a[p+1]===a[q+1]&&a[p+2]===a[q+2]&&a[p+3]===a[q+3]
+ let N=function(x,i,n,r,p){
+  if(n<=60){r[p]=(n-1)<<2;p++}else if(n<256){r[p]=60<<2;r[p+1]=n-1;p+=2}else{r[p]=61<<2;r[p+1]=(n-1)&255;r[p+2]=(n-1)>>>8;p+=3}
+  r.set(x.subarray(i,i+n),p);return p+n}
+ let C=function(r,p,o,n){
+  if(n<12&&o<2048){r[p]=1+((n-4)<<2)+((o>>>8)<<5);r[p+1]=o&255;return p+2}
+  else{r[p]=2+((n-1)<<2);r[p+1]=o&255;r[p+2]=o>>>8;return p+3}}
+ let A=function(r,p,o,n){while(n>=68){p=C(r,p,o,64);n-=64}
+  if(n>64){p=C(r,p,o,60);n-=60};return C(r,p,o,n)}
+ let F=function(x,p,s,r,q){ //xpsrqthlcmofigenbadjkuvwy
+  let t=1;while((1<<t)<=s&&t<=14)t++;t--
+  let h=32-t;if(typeof G[t]==='undefined'){G[t]=new Uint16Array(1<<t)}
+  let g=G[t];g.fill(0)
+  let e=p+s,l,b=p,n=p,a,d,j,c,k,u,v,m,o,w,y,f=1
+  if(s>=15){l=e-15;p++;d=H(L(x,p),h)
+   while(f){k=32;j=p
+    do{p=j;a=d;u=k>>>5;k++;j=p+u;if(p>l){f=0;break};d=H(L(x,j),h);c=b+g[a];g[a]=p-b}while(!E(x,p,c))
+    if(!f)break;q=N(x,n,p-n,r,q)
+    do{v=p;m=4;while(p+m<e&&x[p+m]===x[c+m])m++;p+=m;o=v-c;q=A(r,q,o,m);n=p;if(p>=l){f=0;break}
+     w=H(L(x,p-1),h);g[w]=p-1-b;y=H(L(x,p),h);c=b+g[y];g[y]=p-b}while(E(x,p,c))
+    if(!f)break;p++;d=H(L(x,p),h)
+  }};if(n<e)q=N(x,n,e-n,r,q);return q}
+ let P=function(v,r,p){do{r[p]=v&127;v=v>>>7;if(v>0)r[p]+=128;p++}while(v>0);return p}
+ let M=x=>32+x.length+Math.floor(x.length/6)
+ let T=function(r,u){let l=u.length,p=0,n=0,s;n=P(l,r,n);while(p<l){s=Math.min(l-p,1<<16);n=F(u,p,s,r,n);p+=s};return r.slice(0,n)}
+ let r=new Uint8Array(M(u));return T(r,u)}
--- kde/file/uncompress.js
+++ kde/file/uncompress.js
@@ -1,14 +1,13 @@
-/* from SnappyJs 0.7.0 Zhipeng Jia (MIT) https://github.com/zhipeng-jia/snappyjs */
-function uncompress(x){ //trusted input
+function uncompress(x){ //from SnappyJs 0.7.0 Zhipeng Jia (MIT) https://github.com/zhipeng-jia/snappyjs
  let L=function(x){let p=0,r=0,s=0,c,v
-  while(s<32&&p<x.length){c=x[p];p+=1;v=c&127;r|=v<<s;if(c<128)return[r,p];s+=7}}
+  while(s<32&&p<x.length){c=x[p];p++;v=c&127;r|=v<<s;if(c<128)return[r,p];s+=7}}
  let D=function(r,x,p){let n=x.length,q=0,c,l,s,o,W=[0,255,0xffff,0xffffff,0xffffffff]
   while(p<n){c=x[p];p++
    if((c&3)===0){l=(c>>>2)+1
     if(l>60){s=l-60;l=x[p]+(x[p+1]<<8)+(x[p+2]<<16)+(x[p+3]<<24);l=(l&W[s])+1;p+=s}
     r.set(x.subarray(p,p+l),q);p+=l;q+=l
    }else{switch(c&3){
-    case 1:l=((c>>>2)&7)+4;o=x[p]+((c>>>5)<<8);p+=1;break
+    case 1:l=((c>>>2)&7)+4;o=x[p]+((c>>>5)<<8);p++;break
     case 2:l=(c>>>2)+1;o=x[p]+(x[p+1]<<8);p+=2;break
     case 3:l=(c>>>2)+1;o=x[p]+(x[p+1]<<8)+(x[p+2]<<16)+(x[p+3]<<24);p+=4;break
     default:break}
