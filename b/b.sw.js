d93abc9d13c5d2447ea525109ae2809b5a0c8a11
U-EMEA\csd80117 <ktye@users.noreply.github.com>
2023-08-11 16:40:48 +0200

    b

---
+++ b/b.sw.js
@@ -0,0 +1,24 @@
+this.addEventListener("install", (event) => {
+ console.log("sw install");
+ event.waitUntil(
+  caches
+   .open("v1")
+   .then((cache) =>
+    cache.addAll([
+     "index.html",
+     "b.svg"
+    ])
+   )
+  )
+})
+
+self.addEventListener('fetch', function(event) {
+ event.respondWith(
+  caches.match(event.request).then(function(response) {
+   return response || fetch(event.request);
+  })
+ );
+});
+
+self.addEventListener("activate",event=>{})
+self.addEventListener("beforeinstallprompt",event=>{})
