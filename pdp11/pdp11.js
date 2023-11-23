dcf8295d024895ac8a4ce5ce0b0a3eba4f2e143f
U-EMEA\csd80117 <ktye@users.noreply.github.com>
2023-11-23 17:05:24 +0100

    up

--- pdp11/pdp11.js
+++ pdp11/pdp11.js
@@ -3,18 +3,17 @@ let rst=(c)=>{M=new Uint16Array(64*1024);M[7]=01000;c.forEach((x,i)=>M[256+i]=x)
 
 let A=(x,a)=>{let r=x&7,y=(6==x&070)?M[-1+M[7]>>1]:0
  //todo pc: if((m&7)==7){..}      //pc see 3-18
- console.log("x?",r)
- if(x==027){a[0]=2;console.log("imm",oct(M[M[7]>>1]));return M[7]}
+ if(x==027){a[0]=2;return M[7]}
  a[0]=2*+(5<((x&070)>>3))
  //     r (r)    (r)+     @(r+) -(r)     @-(r) x(r)     @x(r) //addressing modes
  return[r,M[D+r],M[D+r]++,     ,--M[D+r],     ,y+M[D+r],     ][x&070]}
 
 let step=()=>{
- console.log("step", "pc", oct(M[7]))
+ //console.log("step", "pc", oct(M[7]))
  let x=M[M[7]>>1],a=[0]
- show(M[7])             ;   console.log("step", "pc", oct(M[7]), "inst", oct(x))
+ show(M[7])             ;   //console.log("step", "pc", oct(M[7]), "inst", oct(x))
  M[7]+=2
- let d=A(x&0377,a);console.log("a0",a[0]);M[7]+=a[0]
+ let d=A(x&0377,a); //console.log("a0",a[0]);M[7]+=a[0]
  switch(x&077700){
  case 05000: M[d]=0     ; return //clr
  case 05200: M[d]++     ; return //inc
