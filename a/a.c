71c1968996bfd995483ecc1ff3285a8c8126a084
elmar <ktye@users.noreply.github.com>
2024-01-10 23:58:32 +0100

    k-incunabulum

---
+++ a/a.c
@@ -0,0 +1,22 @@
+#include"a.h"//cc -Os -oa a.c -w
+F(w_,write(1,y,x))f(w,127>x?w_(1,(u)&x):w_(strlen(x),x))F(err,w(x);w(58);w(y);w(10);96)f(qz,Qz(1)0)
+G(m,(u)memcpy((void*)x,(void*)y,f))
+f(a,char*s=malloc(x+1);*s++=x;(u)s)
+f(ind,Qr(!ax)x=a(x);i(nx,xi=i)x)
+f(cat,Qr(!ax)u r=a(1);*sr=x;r)
+F(Cat,x=ax?cat(x):x;y=ay?cat(y):y;u r=a(nx+ny);m(ny,r+nx,y);m(nx,r,x))
+f(at,Qr(ax)*sx)
+F(At,Qr(ax||!ay)sx[y])
+f(cnt,Qr(ax)nx)
+F(Cnt,Qr(!ax||ay)x=a(x);m(nx,x,y))
+F(Add,ax?ay?x+y:Add(y,x):r(a(ny),i(nr,ri=(ax?x:xi)+yi)))
+u(*f[])()={0,qz,ind,cnt,cat,at},(*F[])()={0,Add,qz,Cnt,Cat,At};
+u U[26];f(n,10>x-48?x-48:U[x-97])
+char*V=" +!#,@";f(v,(strchr(V,x)?:V)-V)
+us(e,u i=*s++;v(i)?x(e(s),Q(x)f[v(i)](x)):x(n(i),*s?y(e(s+1),Q(y)F[v(*s)](x,y)):x))
+us(ws,u x=*s?e(s):0;Q(x)ax?w(48+x):_(i(nx,w(48+xi);w(32))0);w(10))
+int main(){char s[99];while(1)w(32),s[read(0,s,99)-1]=0,ws(s);}
+
+
+//wasm needs
+char *kinit(){char s[99];w(32);return s;}
