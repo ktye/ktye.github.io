7d260cba8dcd90289e6b045ae95ff81c2273e0ee
U-EMEA\csd80117 <ktye@users.noreply.github.com>
2024-05-15 17:16:12 +0200

    xk

--- plot.js
+++ plot.js
@@ -2,7 +2,7 @@ let plotjs=(()=>{
 
 let ge=function(x){return document.getElementById(x)}
 let pd=function(e){e.preventDefault();e.stopPropagation()}
-let vmin=x=>Math.min(...x),vmax=x=>Math.max(...x),min=Math.min,max=Math.max,floor=Math.floor,ceil=Math.ceil,abs=Math.abs
+let vmin=x=>Math.min(...x.filter(x=>!isNaN(x))),vmax=x=>Math.max(...x.filter(x=>!isNaN(x))),min=Math.min,max=Math.max,floor=Math.floor,ceil=Math.ceil,abs=Math.abs
 
 function replot(canvas){plots(canvas.plots,canvas,canvas.slider,canvas.listbx,canvas.legend)}
 
@@ -30,13 +30,14 @@ function plots(p,canvas,slider,listbx,legend){const z=devicePixelRatio //zoom sc
  if(p.hi.point!==null){
   let pl=p.plots[p.hi.plot].lines[p.hi.lines[0]],pt=p.hi.point
   let d={x:("undefined"!=typeof pl.x)?pl.x[pt]:null,y:("undefined"!=typeof pl.y)?pl.y[pt]:null,z:("undefined"!=typeof pl.z)?pl.z[pt]:null}
-  d.r=Math.hypot(d.y,d.z);d.a=180/Math.PI*Math.atan2(d.z,d.y);if(d.a<0)d.a+=360
+  d.r=Math.hypot(d.y,d.z);d.a=180/Math.PI*Math.atan2(d.y,d.z);if(d.a<0)d.a+=360
   let s=((d.x!==null)?("x:"+sn(d.x)+";"):"")+((d.z!==null)?("z:"+sn(d.r)+"a"+d.a.toFixed(0)):("y:"+sn(d.y)))
   legend(s)
  }
- let clickpos=e=>{let r=e.target.getBoundingClientRect(),x=e.clientX-r.left,y=e.clientY-r.top,ri=targetRect(x,y,rects);return[x,y,ri]}
- canvas.ondblclick=function(e){pd(e);let[x,y,ri]=clickpos(e)
-  let [line,point]=snapPoint(x,y,rects[ri],p.plots[ri])
+ let clickpos=e=>{let r=e.target.getBoundingClientRect(),z=devicePixelRatio,x=z*(e.clientX-r.left),y=z*(e.clientY-r.top),ri=targetRect(x,y,rects);return[x,y,ri]}
+ canvas.ondblclick=function(e){pd(e);zoom=null;canvas.style.cursor="";let[x,y,ri]=clickpos(e),l=p.plots[ri].limits,rri=rects[ri],[X,Y]=rects[ri][5](x-rri[0],y-rri[1])
+  if(outside(l,X,Y)){delete p.plots[ri].limits;replot(canvas);return}
+  let[line,point]=snapPoint(x,y,rects[ri],p.plots[ri])
   p.hi={plot:ri,lines:[line],point:point}
   if(slider!==undefined){
    slider.min=0;slider.max=p.plots[ri].lines[line].y.length-1
@@ -45,16 +46,9 @@ function plots(p,canvas,slider,listbx,legend){const z=devicePixelRatio //zoom sc
   replot(canvas)
  }
  if(slider!==undefined){
-  let I=Math.ceil(slider.max/5)
-  slider.f.style.display=(p.hi.point===null)?"none":""
-  slider.b.style.display=(p.hi.point===null)?"none":""
-  let f=x=>e=>{p.hi.point+=x*(1+1000*(e.shiftKey||e.ctrlKey));p.hi.point=(p.hi.point<0)?0:(p.hi.point>slider.max)?slider.max:p.hi.point;replot(canvas)}
-  slider.f.onclick   =f( 1)
-  slider.b.onclick   =f(-1)
-  slider.f.ondblclick=f( I)
-  slider.b.ondblclick=f(-I)
-  /* slider.onchange=function(e){ p.hi.point=slider.value;replot(canvas) } */
- }
+  let I=Math.ceil(slider.max/10);slider.f.style.display=(p.hi.point===null)?"none":"";slider.b.style.display=(p.hi.point===null)?"none":""
+  let f=x=>e=>{console.log("delta",e.deltaY);p.hi.point+=(e.deltaY===undefined)?x:(e.deltaY<0)?x:-x;p.hi.point=(p.hi.point<0)?0:(p.hi.point>slider.max)?slider.max:p.hi.point;replot(canvas)}
+  slider.f.onclick=f(1);slider.b.onclick=f(-1);slider.f.onwheel=f(I);slider.b.onwheel=f(I)}
  if(listbx!==undefined){
   //rm(listbox)
   //let empty=(!"table"in p)
@@ -72,15 +66,15 @@ function plots(p,canvas,slider,listbx,legend){const z=devicePixelRatio //zoom sc
  let drawzoom=(x0,y0,x1,y1,ri)=>{if(zoom.hv)[x1,y1]=(abs(x1-x0)>abs(y1-y0))?[x1,y0]:[x0,y1];  c.putImageData(zoom.d,0,0);zoom.pan?LN(c,x0,y0,x1,y1):c.strokeRect(x0,y0,x1-x0,y1-y0)}
  let startzoom=(pan,hv,meas,x0,y0,X,Y,ri)=>{if(ri==undefined)return;zoom={x0:x0,y0:y0,X:X,Y:Y,ri:ri,d:c.getImageData(0,0,canvas.width,canvas.height),pan:pan,hv:hv&&p.plots[ri].type!="polar",meas:meas};c.strokeStyle="red";canvas.style.cursor=pan?"grab":"crosshair"}
  let endzoom=(_x,_y,X,Y,ri)=>{
-  canvas.style.cursor="";c.putImageData(zoom.d,0,0);if(zoom.ri!=ri){zoom=null;return}; 
+  canvas.style.cursor="";c.putImageData(zoom.d,0,0);if(zoom.ri!=ri){zoom=null;return};
   let x=zoom.X,y=zoom.Y,dx=x-X,dy=y-Y,l=p.plots[ri].limits;if(outside(l,x,y)&&outside(l,X,Y)){zoom=null;return};
-  if(zoom.pan){if(zoom.hv){[dx,dy]=(abs(dx)>abs(dy))?[dx,0]:[0,dy];if(zoom.meas){zoom=null;legend(["dx","dy"][+(dy>dx)]+": "+(dx+dy));return}};
-  if(zoom.meas){legend("dz: "+absang(-dx,-dy));zoom=null;return};l[0]+=dx;l[1]+=dx;l[2]+=dy;l[3]+=dy}
+  if(zoom.pan){if(zoom.hv){[dx,dy]=(abs(dx)>abs(dy))?[dx,0]:[0,dy];if(zoom.meas){zoom=null;legend(["dx","dy"][+(dy>dx)]+": "+abs(dx+dy));return}};
+  if(zoom.meas){legend("dz: "+absang(-dx,dy));zoom=null;return};l[0]+=dx;l[1]+=dx;l[2]+=dy;l[3]+=dy}
   else p.plots[ri].limits=[min(x,X),max(x,X),min(y,Y),max(y,Y)];zoom=null;replot(canvas)
  }
  canvas.onmousemove=e=>{pd(e);if(1==e.which){return(zoom===null)?startzoom(e.shiftKey||e.ctrlKey,e.ctrlKey,e.shiftKey&&e.ctrlKey,...mousecoords(e)):drawzoom(zoom.x0,zoom.y0,...clickpos(e))};if(zoom===null)return;if(e.which==0)endzoom(...mousecoords(e)) }
  canvas.onwheel=e=>{let[x,y,X,Y,ri]=mousecoords(e);if(e.deltaY==0)return;let z=(e.deltaY>0)?2:0.5,pi=p.plots[ri],l=pi.limits,xm=(l[0]+l[1])/2,ym=(l[2]+l[3])/2,DX=(l[1]-l[0])/2*z,DY=(l[3]-l[2])/2*z
-  if(pi.type=="polar"){if(DX>abs(xm)&&DY>abs(ym)){xm=0;ym=0};l[0]=xm-DX;l[1]=xm+DX;l[2]=ym-DY;l[3]=ym+DY}else{let xo=(X-xm)*z,yo=(Y-ym)*z;if(X>l[0]){l[0]=X-xo-DX;l[1]=X-xo+DX};if(Y>l[2]){l[2]=Y-yo-DY;l[3]=Y-yo+DY}}
+  let xo=(X-xm)*z,yo=(Y-ym)*z;if(X>l[0]){l[0]=X-xo-DX;l[1]=X-xo+DX};if(Y>l[2]){l[2]=Y-yo-DY;l[3]=Y-yo+DY}
   replot(canvas)}
 }
 
@@ -133,18 +127,18 @@ function polarplot(p,c,w,h,plts){if(!p.lines)return;
  ln=(l,i)=>{let x=l.y,y=l.z;if(!x.length)return
   let s=hiline(plts,i)*(l.size?l.size:3);c.fillStyle=plts.colors[i%plts.colors.length]
   let hp=j=>hipoint(plts,i,j,p.i)
-  x.map((x,i)=>{c.beginPath();c.arc(sc*(x-X),sc*(y[i]-Y),s*hp(i),0,2*Math.PI);c.fill()})}
+  x.map((x,i)=>{c.beginPath();c.arc(sc*(x-X),-sc*(y[i]-Y),s*hp(i),0,2*Math.PI);c.fill()})}
  p.limits=li;X=(li[0]+li[1])/2,Y=(li[2]+li[3])/2
  c.translate(xm,ym)
  circle(c,0,0,ra)
  c.textAlign="center";c.textBaseline="bottom"
  c.fillText(p.title,0,-ra-plts.tl-plts.tlh)
- align(c,0);c.fillText("↖"/*"⮤"*/+sn(r1),floor(0.74*ra),floor(0.67*ra));if(X!=0||Y!=0)c.fillText("-"+absang(X,Y),floor(0.74*ra),floor(plts.tih+0.67*ra))
+ align(c,0);c.fillText("↖"/*"⮤"*/+sn(r1),floor(0.74*ra),floor(0.67*ra));if(X!=0||Y!=0)c.fillText("-"+absang(X,-Y),floor(0.74*ra),floor(plts.tih+0.67*ra))
  if(p.ylabel){align(c,0);c.fillText(p.ylabel,floor(0.74*ra),plts.tih+floor(0.67*ra))}
  c.font=plts.font2;iota(12).map(i=>{let p=30*i*Math.PI/180,x=Math.sin(p),y=-Math.cos(p);align(c,al[i]);c.beginPath();c.moveTo(ra*x,ra*y);c.lineTo(raa*x,raa*y);c.stroke();c.fillText(""+30*i,raa*x,raa*y)})
  c.strokeStyle="#ccc";ncTic(0,r1).slice(1,-1).map(t=>circle(c,0,0,sc*t))
  c.beginPath();c.moveTo(-ra,0);c.lineTo(ra,0);c.stroke();c.beginPath();c.moveTo(0,-ra);c.lineTo(0,ra);c.stroke()
- c.beginPath();c.arc(0,0,ra,0,2*Math.PI);c.clip();p.lines.map(ln);return[(l,j)=>[floor(w/2)+sc*l.y[j],floor(h/2)+floor(plts.tih/2)+sc*l.z[j]],(x,y)=>[X+(x-xm)/sc,Y+(y-ym)/sc]]
+ c.beginPath();c.arc(0,0,ra,0,2*Math.PI);c.clip();p.lines.map(ln);return[(l,j)=>[floor(w/2)+sc*(l.y[j]-X),floor(h/2)+floor(plts.tih/2)-sc*(l.z[j]-Y)],(x,y)=>[X+(x-xm)/sc,Y+(ym-y)/sc]]
 }
 function ampangplot(p,c,w,h,plts){p.lines.forEach((l,i)=>{l.y.forEach((y,j)=>{let z=l.z[j],a=Math.atan2(y,z)*180/Math.PI;l.y[j]=Math.hypot(y,z);l.z[j]=(a<0)?a+360:a});p.lines[i]=l});p.type="xy";return xyplot(p,c,w,h,plts)}
 function barplot(p,c,w,h,plts){p.bar=true;p.type="xy";return xyplot(p,c,w,h,plts)}
@@ -178,13 +172,9 @@ function snapPoint(x,y,rect,p){x-=rect[0];y-=rect[1]
  for(let i=0;i<p.lines.length;i++){
   let l=p.lines[i]
   for(let j=0;j<l.y.length;j++){
-   let [X,Y]=f(l,j);X-=x;Y-=y
-   let d=X*X+Y*Y
-   if(d<dist){dist=d;line=i;point=j}
-  }
- }
- return [line,point]
-}
+   let [X,Y]=f(l,j);X-=x;Y-=y;let d=X*X+Y*Y
+   if(d<dist){dist=d;line=i;point=j}}}
+ return[line,point]}
 
 function align(c,x){c.textBaseline=["top","middle","bottom"][floor(x/3)];c.textAlign=["left","center","right"][x%3]}
 function center(x){return floor(x/2)}
@@ -193,7 +183,7 @@ function text90(c,x,y,t){c.save();c.translate(x,y);c.rotate(-Math.PI/2);c.fillTe
 function limits(p){let r=[0,0,0,0,0,0],l=p.limits;if("undefined"===typeof l)return r
  switch(l.length){case 0:return r;case 1:return[0,0,0,l[0],0,0];case 2:return[l[0],l[1],0,0,0,0];
  case 3:return[l[0],l[1],0,l[2],0,0];case 4:return l.concat([0,0]);default:return l}}
-function autoxy(L,a,bar){let r=a.slice(0,4)
+function autoxy(L,a,bar){let r=a.slice(0,4);
  if(r[0]==r[1]){[r[0],r[1]]=ncLim(vmin(L.map(l=>vmin(l.x))),vmax(L.map(l=>vmax(l.x)))).slice(0,2);if(bar){let bw=barwidth(L);r[0]-=bw/2;r[1]+=bw/2}}
  if(r[2]==r[3]){[r[2],r[3]]=ncLim(vmin(L.map(l=>vmin(l.y))),vmax(L.map(l=>vmax(l.y)))).slice(0,2);if(bar){r[2]=min(0,r[2]);r[3]=max(0,r[3])}}
  return r}
