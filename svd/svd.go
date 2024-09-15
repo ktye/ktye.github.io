5ca06fae4f13fc6a5b329520edd715bac40728a6
elmar <ktye@users.noreply.github.com>
2024-09-15 21:56:54 +0200

    up

--- svd/svd.go
+++ svd/svd.go
@@ -14,62 +14,40 @@ func main() {
 		Z{2 + 1i, 3 - 7i, 3 + 2i, 2 + 4i},
 	}
 	jaco_test(A[1], A[2])
-	/*
-		U, V, s := jaco(A)
-		pmat("A", U)
-		pmat("U", U)
-		pmat("V", U)
-		fmt.Println(s)
-		fmt.Println("UU", unitary(U))
-		fmt.Println("VV", unitary(V))
-	*/
+
+	U, V, s := svd(A) //U,V,A: list of columns
+	pmat("A", U)
+	pmat("U", U)
+	pmat("V", V)
+	fmt.Println("s", s)
+	fmt.Println("UU", unitary(U))
+	fmt.Println("VV", unitary(V))
 }
 
 type M [][]complex128 // nxn
 type R []float64
 type Z []complex128
 
-/*
-func jaco(u, v Z) (cs, sn float64) {
-	a := (hot(v, v) - hot(u, u)) / (2*hot(u, v))
-	t := math.Sign(a) / (math.Abs(a) + math.Sqrt(1+a*a))
-	cs := 1.0 / math.Sqrt(1+t*t)
-	sn := t*cs
-	return cs, sn
-}
-*/
-
 func jaco_test(u, v Z) {
-	//	for i := range u {
-	//u[i] = complex(real(u[i]), 0)
-	//v[i] = complex(real(v[i]), 0)
-	//	}
-	cs, sn := jaco(u, v)
-	fmt.Println("cs/sn", cs, sn)
-	//sm := -cmplx.Conj(sn)
+	cs, SN := jaco(u, v)
+	fmt.Println("cs/sn", cs, SN)
 
-	A := real(hot(u, u))
+	A := hot(u, u)
 	B := hot(u, v)
-	C := real(hot(v, v))
-	// [cs sn';-sn cs]*[A B;B' C]*[cs -sn';sn cs] = [l1 0;0 l2]
+	C := hot(v, v)
+	CS := complex(cs, 0)
 
-	z := func(r float64) complex128 { return complex(r, 0) }
-	x := M{Z{z(A), B}, Z{cmplx.Conj(B), z(C)}}
-	xx := mul2(x, M{Z{z(cs), -cmplx.Conj(sn)}, Z{sn, z(cs)}})
-	xxx := mul2(M{Z{z(cs), cmplx.Conj(sn)}, Z{-sn, z(cs)}}, xx)
+	// T=[cs -sn;sn' c] digonalizes B=[A B;B' C] with T*B*T'
+	T := M{Z{CS, -SN}, Z{cmplx.Conj(SN), CS}}
+	TT := trans(T)
 
-	fmt.Println("M", x)
-	fmt.Println("MMM", xxx)
+	x := M{Z{A, B}, Z{cmplx.Conj(B), C}}
+	x = mul2(x, TT)
+	x = mul2(T, x)
+	fmt.Println("x", x)
 
-	/*
-	   x := add(scale(cs, u), zcale(sm, v))
-	   y := add(zcale(sn, u), scale(cs, v))
-	   fmt.Println("u", u)
-	   fmt.Println("v", v)
-	   fmt.Println("x", x)
-	   fmt.Println("y", y)
-	   fmt.Println("x'y", hot(x, y))
-	*/
+	U, V := rot(u, v, cs, SN)
+	fmt.Println("U'V", hot(U, V))
 }
 func mul2(A, B M) (M M) { // cik=aij*bjk
 	M = mat(len(A))
@@ -80,26 +58,26 @@ func mul2(A, B M) (M M) { // cik=aij*bjk
 	return M
 }
 
-// eigenvectors of 2x2 hermitian from columns u and v
-// [A  B;B' C]           B': conj(B)
-// A:u'*u, C:v'*v, B:u'*v
-// [cs sn';-sn cs]*[A B;B' C]*[cs -sn';sn cs] = [l1 0;0 l2]
+// T=[cs -sn;sn' c] digonalizes B=[A B;B' C] with T*B*T'
 func jaco(u, v Z) (cs float64, sn complex128) {
 	A := real(hot(u, u))
 	B := hot(u, v)
 	C := real(hot(v, v))
-	b := cmplx.Abs(B)
-	w := complex(real(B)/b, -imag(B)/b) //cmplx.Conj(B) / b
-	q := (C - A) / (2 * b)
+	q := (C - A) / (2 * cmplx.Abs(B))
 	t := sign(q) / (math.Abs(q) + math.Sqrt(1+q*q))
 	cs = 1.0 / math.Sqrt(1+t*t)
-	return cs, complex(cs*t, 0) * w
+	sn = B * complex(t*cs/cmplx.Abs(B), 0)
+	return cs, sn // d:+/conj*;A:_d[u;u];B:d[u;v];C:_d[v;v];s:(B%abs B)*t*c:1%%1+t*t:((q>0)-q<0)%(abs q)+%1+q*q:(C-A)%2*abs B
+}
+func rot(u, v Z, cs float64, SN complex128) (U, V Z) {
+	U = add(scale(cs, u), zcale(-cmplx.Conj(SN), v))
+	V = add(zcale(SN, u), scale(cs, v))
+	return U, V
 }
 
-/*
-func jaco(A M) (U, V M, sigma R) { //A:list of columns (A[i]: column i)
+func svd(A M) (U, V M, sigma R) { //A:list of columns (A[i]: column i)
 	n, e := len(A), 1.0
-	V, U, x := mat(n), mat(n), make(Z, n)
+	V, U = mat(n), mat(n)
 	for i := range V {
 		V[i][i] = 1
 	}
@@ -107,59 +85,29 @@ func jaco(A M) (U, V M, sigma R) { //A:list of columns (A[i]: column i)
 		e = 0
 		for i := range A {
 			for k := 1 + i; k < n; k++ {
-				ii := hot(A[i], A[i])
-				kk := hot(A[k], A[k])
-				ik := hot(A[k], A[i])
-				if ee := cmplx.Abs(ik) / math.Sqrt(ii*kk); ee > e {
+				u, v := A[i], A[k]
+
+				a := real(hot(u, u))
+				b := hot(u, v)
+				c := real(hot(v, v))
+				if ee := cmplx.Abs(b) / math.Sqrt(a*c); ee > e {
 					e = ee
 				}
-				q := (kk - ii) / (2 * ik)
-				t := math.Sign(q) / (math.Abs(q) + math.Sqrt(1+q*q))
-				cs := 1.0 / math.Sqrt(1+t*t)
-				sn := cs * t
 
-				copy(x, A[i])
-				A[i] = add(scale(cs, x), scale(sn, A[k]))
-				A[k] = add(scale(sn, x), scale(-cs, A[k]))
-
-				copy(x, V[i])
-				V[i] = add(scale(cs, x), scale(sn, V[k]))
-				V[k] = add(scale(sn, x), scale(-cs, V[k]))
+				cs, sn := jaco(u, v)
+				A[i], A[k] = rot(u, v, cs, sn)
+				V[i], V[k] = rot(V[i], V[k], cs, sn)
 			}
 		}
 	}
+	sigma = make(R, n)
 	for i := range A {
 		sigma[i] = norm(A[i])
 		U[i] = scale(1.0/sigma[i], A[i])
 	}
 	return U, V, sigma
 }
-*/
 
-// golub-kahan-lanczos for nxn
-// UH A V = [a0 b0 0..;0 a1 b1 0..;.. ai bi..] (bi-diagonal)
-// A Vk = Uk Bk
-// UH U = I, Vk V = I
-func bidi(A M) (U, V M, a, b R) {
-	n := len(A)
-	a, b, U, V = make(R, n), make(R, n), mat(n), mat(n)
-	H := trans(A)
-	V[0][0] = 1
-	for k := range A {
-		U[k] = mvec(A, V[k])
-		if k > 0 {
-			U[k] = add(U[k], scale(-b[k-1], U[k-1]))
-		}
-		a[k] = norm(U[k])
-		U[k] = scale(1/a[k], U[k])
-		if k < len(A)-1 {
-			v := add(mvec(H, U[k]), scale(-a[k], V[k]))
-			b[k] = norm(v)
-			V[1+k] = scale(1/b[k], v)
-		}
-	}
-	return U, V, a, b[1:]
-}
 func add(x, y Z) (r Z) {
 	r = make(Z, len(x))
 	for i := range x {
