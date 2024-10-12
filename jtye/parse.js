26248948218da1ceb9edbc99677d5340be601e93
elmar <ktye@users.noreply.github.com>
2024-10-12 12:49:25 +0200

    up

--- jtye/parse.js
+++ jtye/parse.js
@@ -1,4 +1,4 @@
-let token=x=>{let C,c,ci=x=>String.fromCharCode(...x),ic=x=>x.split("").map(x=>x.charCodeAt()),
+let token=x=>{let C,c,i=x=>split(x,"").map(x=>x.charCodeAt()),
  S=" \t",                    //space
  L="({[;\n",                 //left
  R=")}]",                    //right
@@ -22,10 +22,10 @@ let token=x=>{let C,c,ci=x=>String.fromCharCode(...x),ic=x=>x.split("").map(x=>x
     "e;)+'11q`1+1'';",
     "rrrrrrrtrrrrsrr",
     "srrrrrrrrrrrrrr",
-    "t;)+'a0q`++a'';"];T=drop(T,1);let t0=ic(join(each(first)(T),""));T=each(x=>find(ic(x).slice(1),t0))(T)
-  C=[ic(S+L),ic(R),ic(V),ic(A),cat(add(65,til(26)),add(97,til(26))),ic("0123456789."),...each(list)(ic("\"`-:e\\/\n"))];
+    "t;)+'a0q`++a'';"];T=drop(T,1);let t0=i(join(each(first)(T),""));T=each(x=>find(i(x).slice(1),t0))(T)
+  C=[i(S+L),i(R),i(V),i(A),cat(add(65,til(26)),add(97,til(26))),i("0123456789."),...each(list)(i("\"`-:e\\/\n"))];
   C=C.reduce((a,x,i)=>amend(i,0,x,a),add(-1,where(256)))
-  return cut(x,where(more(scan((x,y)=>T[y][x],0)(at(ic(x),C)),10)))},
+  return cut(x,where(more(scan((x,y)=>T[y][x],0)(at(i(x),C)),10)))},
   
  
 
@@ -35,17 +35,17 @@ parse=s=>{s=((s.constructor===String)?token(s):s).filter(x=>!((1<x.length&&s[x]=
  s=s.map(x=>new String(x))                                        //we use String object to mark verbs instead of primitive strings
  
  let V=":+-*%&|<>=~.!@?^#_,$'/\\"
- 
- let f1=" type neg sqr sqrt flip rev up down frq not value til first uniq sort count floor list string each right left".split(" ")
- let f2=" add sub mul div min max less more eql match dot dict at find cut take drop cat string both join split".split(" ")
- let F=(x,f)=>{let i=V.indexOf(x);return -1<i?f[i]:x},mo=(x,y)=>b(F(x,f1),y),dy=(x,y,z)=>b(F(x,f2),[y,z])
+ f1=" type neg sqr sqrt flip rev up down frq not value til first uniq sort count floor list string each right left".split(" "),
+ f2=" add sub mul div min max less more eql match dot dict at find cut take drop cat string both join split".split(" "),
+ a2=(x,y)=>(y=["","both","right","left"][1+["each","over","scan"].indexOf(x.slice(0,4))])?y+x.slice(4):x,
+ F=(x,f)=>{let i=V.indexOf(x);return -1<i?f[i]:x},mo=(x,y)=>b(F(x,f1),y),dy=(x,y,z)=>b(F(a2(x),f2),[y,z])
  
  
  let t=()=>{let r                                           //t(): term
   if(!L(s))return 0;r=s.pop();                              //next token
   if(i(")}];",r)){s.push(r);return 0}                       //terminator
   r.verb=i(V,r)
-  if(r=="(")r=R()                                           //(1) and (1;2;3)->[3;2;1]
+  if(r=="(")r=R()                                           //(1) and (1;2;3)
   while(1){                                                 //adverb and [application]
    if(!L(s))return r;let p=s.pop()
    if(i("'/\\",p))r=d(p,r)
@@ -54,11 +54,11 @@ parse=s=>{s=((s.constructor===String)?token(s):s).filter(x=>!((1<x.length&&s[x]=
   }
   return r},
  
- e=(x)=>{let y,z                                          //e(t()): expr
+ e=(x)=>{let y,z                                           //e(t()): expr
   if(!x)return 0;if(!(y=t()))return x
   if(y.verb&&!x.verb){
    z=e(t())
-   return L(y)==":"?$(x,y,z):(!z)?p(x,y):z.verb?q(x,y,z):dy(y,z,x)
+   return L(y)==":"?_(x,y,z):(!z)?p(x,y):z.verb?q(x,y,z):dy(y,z,x)
   }
   z=e(y);return(!x.verb)?j(z,x):z.verb?c(x,z):mo(x,z)},
  
@@ -73,7 +73,7 @@ parse=s=>{s=((s.constructor===String)?token(s):s).filter(x=>!((1<x.length&&s[x]=
  q=(x,y,z)=>v(B(i(V,z)?b("(y,x)=>"+F(y,f2),[b(F(z,f2),"y,x"),x]):b("x=>"+F(y,f2),[b(F(z,f1),"x"),x]))),  //compose project 1+-
  l=r=>{r=[];while(L(s)){r.push(e(t()));if(s.pop()!=";")return r}},L=x=>x.length?x[x.length-1]:0,    // list e;e;e[terminator]
  R=()=>{let r=l();r=1==r.length?n(r[0]):"rev(["+r.reverse().join(",")+"])";return r},
- $=(x,y,z)=>y[0]==":"?(x+"="+z):x+"=?amend?"
+ _=(x,y,z)=>y[0]==":"?(x+"="+z):x+"=?amend?"
  //λ 
  
  s.reverse()
