384e6cb1e285449032301d26f9b0226dae727088
elmar <ktye@users.noreply.github.com>
2024-10-27 21:24:59 +0100

    up

--- jtye/parse.js
+++ jtye/parse.js
@@ -12,7 +12,7 @@ let token=x=>{let C,c,i=x=>split(x,"").map(x=>x.charCodeAt()),
     "a;)+'bbq`++b'';",
     "0;)+'11q`+:e'';",
     "qrrrrrrtrrrrsrr",
-    "`jjjjjjjjjjjjjj",  // "`;)+'b0q`++b'';",
+    "`jjjjjjjjjjjjjj",
     "-;)+'a1q`-+a'';",
     "/ccccccccccccc;",
     "cccccccccccccc;",
@@ -26,12 +26,13 @@ let token=x=>{let C,c,i=x=>split(x,"").map(x=>x.charCodeAt()),
     "t;)+'a0q`++a'';"];T=drop(T,1);let t0=i(join(each(first)(T),""));T=each(x=>find(i(x).slice(1),t0))(T)
   C=[i(S+L),i(R),i(V),i(A),cat(add(65,til(26)),add(97,til(26))),i("0123456789."),...each(list)(i("\"`-:e\\/\n"))];
   C=C.reduce((a,x,i)=>amend(i,0,x,a),add(-1,where(256)))
-  return cut(x,where(more(scan((x,y)=>T[y][x],0)(at(i(x),C)),10)))},
+  return cut(x=x.trim(),where(more(scan((x,y)=>T[y][x],0)(at(i(x),C)),10))).map(x=>x=="\n"?";":x)},
   
 
-parse=$=>{$=(($.constructor===String)?token($):$).filter(x=>!((1<x.length&&$[x]=="/")||(!x.trim().length))) //remove comments,space
- let k=scan((x,y)=>x+x*y)($.map(x=>+!isNaN(parseFloat(x)))),ax=x=>x.constructor===Array                     //join numeric vectors
- $=$.reduce((a,x,j)=>{a.push(1==k[j]?[x]:1<k[j]?[...a.pop(),x]:x);return a},[]).map(x=>(ax(x)&&1==x.length)?x[0]:ax(x)?("["+x.join(",")+"]"):x)
+parse=$=>{$=(($.constructor===String)?token($):$).filter(x=>!((1<x.length&&x[0]=="/")||!x.trim().length)) //rm space&comments
+ let k=scan((x,y)=>x+x*y)($.map(x=>+!isNaN(parseFloat(x)))),ax=x=>x.constructor===Array,                     
+ b=(x,y,s,q)=>x+(q?q[0]:"(")+((y.constructor===Array)?y.join(s?s:","):y)+(q?q[1]:")"),B=x=>b("",x)      //join numeric vectors
+ $=$.reduce((a,x,j)=>{a.push(1==k[j]?[x]:1<k[j]?[...a.pop(),x]:x);return a},[]).map(x=>(ax(x)&&1==x.length)?x[0]:ax(x)?b("",x,",","[]"):x)
  $=$.map(x=>new String(x))                                        //we use String object to mark verbs instead of primitive strings
  
  let V=":+-*%&|<>=~.!@?^#_,$'/\\",xyz=[],
@@ -60,32 +61,29 @@ parse=$=>{$=(($.constructor===String)?token($):$).filter(x=>!((1<x.length&&$[x]=
   if(!x)return 0;if(!(y=t()))return G(x)     //x or primitive
   if(y.verb&&!x.verb){
    z=e(t())
-   return(!z)?p(x,y):z.verb?c(p(x,y),z):sw(z,"id(")?_(x,y,sl(z,3)):y==":"?_(x,0,z):D(y,z,x)
+   return(!z)?p(x,y):y==":"?_(x,0,z):z.verb?c(p(x,y),z):sw(z,"id(")?_(x,y,sl(z,3)):D(y,z,x)
   }
   z=e(y);return(!x.verb)?j(z,x):z.verb?c(x,z):M(x,z)},
   
  i=(x,y)=>x.includes(S(y)),sw=(x,y)=>x.startsWith(y),sl=(x,y)=>x.slice(y,-1),
  a=(x,y,n)=>x in J?((n=cut(y=l(),[0,J[x]])),x+(J[x]?b("",n[0],";"):"")+(n[1].length?b("",n[1],";","{}"):"")+(0==J[x]?e(t()):"")):
-  1==(n=(y=l()).length)?b("at",[y[0],F(x,f1)]):"$"==x&&y.length>2?C(y):y.undef?b("curry",F(x,f2)+",["+y.reverse().join(",")+"]"):b(F(x,f2),y.reverse()), //apply x[y] or x[y;z;..]
- b=(x,y,s,q)=>x+(q?q[0]:"(")+((y.constructor===Array)?y.join(s?s:","):y)+(q?q[1]:")"),B=x=>b("",x),                        //brace x(y) or x(y0,y1)
+  1==(n=(y=l()).length)?b("at",[y[0],F(x,f1)]):"$"==x&&y.length>2?C(y):y.undef?b("curry",F(x,f2)+b(",",y.reverse(),",","[]")):b(F(x,f2),y.reverse()), //apply x[y] or x[y;z;..]
  C=x=>B(sl(join(x.map((x,i)=>x+"?:"[i&1]),""),0)), //cond
  c=(x,y)=>v(B("(x,y)=>"+(i(f2,y)?cx(x,F(y,f2)+"(x,y)"):sw(y,"(x=>")?cx(x,sl(y,4)):sw(y,"((x,y)=>")?cx(x,sl(y,8)):cx(x,y+"(x)")))),
  cx=(x,y)=>sw(x,"(x=>")?sl(x,4).replace("(x,","("+y+","):b(i(V,x)?F(x,f1):x,y),                  //c:composition
- d=(x,r)=>v( (x=="'"&&i(V,r)&&V.indexOf(r)<11)?b("prior",F(r,f2)):   ["each(","over(","scan("]["'/\\".indexOf(x)]+(x=="'"?H(r):G(r))+")"),                //derived: +/ -> over(add)
+ d=(x,r)=>v( (x=="'"&&i(V,r)&&V.indexOf(r)<11)?b("prior",F(r,f2)):["each(","over(","scan("]["'/\\".indexOf(x)]+(x=="'"?H(r):G(r))+")"),                //derived: +/ -> over(add)
  M=(x,y)=>b(F(x,f1),y),D=(x,y,z)=>b(F(a2(x),f2),[y,z]),j=(x,y)=>b("at",[x,y]),                   //monadic/dyadic/juxtaposition
  p=(x,y)=>v(B(b("x=>"+F(y,f2),["x",x]))),                                                        //project 1+
- v=x=>((x=new String(x)).verb=true,x),S=x=>x.toString(),n=x=>((x=new String(x)).verb=false,x),   //mark verb/noun
- l=(r,x)=>{r=[];while(L($)){r.push((x=e(t()))?x:(r.undef=1,"undefined"));if($.pop()!=";")return r}},L=x=>x.length?x[x.length-1]:0, //list e;e;e[terminator]
+ v=x=>((x=new String(x)).verb=true,x),S=x=>String(x),n=x=>((x=new String(x)).verb=false,x),   //mark verb/noun
+ l=(r,x)=>{r=[];while(L($)){r.push((x=e(t()))?x:(r.undef=1,"undefined"));if(";"!=$.pop())return r}},L=x=>x.length?x[x.length-1]:0, //list e;e;e[terminator]
  R=()=>{let r=l();r=1==r.length?n(r[0]):"rev(["+r.reverse().join(",")+"])";return r},
  G=x=>i(V,x)?v(F(x,f2)):x,                                                                                //- -> neg
  H=x=>i(V,x)?("((x,y)=>y===undefined?"+F(x,f1)+"(x):"+F(x,f2)+"(x,y))"):i(f2,x)?H(V[f2.indexOf(S(x))]):x, //neg -> ambivalent
  _=(x,y,z,s,i)=>B(sw(x,"at")?((s=sl(x,1+(i=x.lastIndexOf(","))))+b("=amend",[z,F(y,f2),x.slice(3,i),s])):x+"="+(y?D(y,z,x):λλλ(x,z))),  //assign(4x:modified&indexed)
- λ=(r,t,X)=>{t=λλ;λλ=[];λλ.n=0;
-  X=xyz;xyz="["==L($)?($.pop(),l().map(S)):["x","y","z"]
-  r=l();if(1<r.length){r[r.length-1]="return "+L(r);r="{"+join(r,";")+"}"}
-  [t,λλ]=[λλ,t];[X,xyz]=[xyz,X];return B(b("",uniq(cat(t,rev(X.slice(0,1+t.n)))))+"=>"+r)},λλ=[],λλλ=(x,y)=>(λλ.push(x),y)
+ λ=(r,t,X)=>{t=λλ;λλ=[];λλ.n=0;X=xyz;xyz="["==L($)?($.pop(),l().map(S)):["x","y","z"];r=l()
+  if(1<r.length){r[r.length-1]="return "+L(r);r=b("",r,";","{}")}
+  [t,λλ]=[λλ,t];[X,xyz]=[xyz,X];return B(b("",uniq(cat(t,rev(X.slice(0,1+t.n)))).filter(x=>!i(x,".")))+"=>"+r)},λλ=[],λλλ=(x,y)=>(λλ.push(x),y)
  
- 
- $.reverse()  //return S(e(t()))
+ while(L($)==";")$.pop();$.reverse()
  return join(l(),";")
 }
