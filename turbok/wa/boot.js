c38b2628bdaee50a36cdd6b6c343df0087dfc0c2
elmar <ktye@users.noreply.github.com>
2025-05-01 19:58:35 +0200

    up

--- turbok/wa/boot.js
+++ turbok/wa/boot.js
@@ -1,5 +1,12 @@
 O("/bootstrap\n")
+fetch("wa/ai.a").then(r=>r.text()).then(a=>{
+ O("$wa ai.a\n")
+ let w=wa(a)
+ O("ai.w ("+w.length+" bytes)\n")
+ A=new WebAssembly.Module(w)
+})
 
+/*
 fetch("wa/t.a").then(r=>r.text()).then(a=>{
  try{
   ea.value=a
@@ -10,3 +17,4 @@ fetch("wa/t.a").then(r=>r.text()).then(a=>{
   A=L(w)
  }catch(e){ O(e.message);throw(e) }
 })
+*/
