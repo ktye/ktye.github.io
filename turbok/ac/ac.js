dd89b0bc96a29ed5119a00c4f5a9dfd07d9d5ec0
elmar <ktye@users.noreply.github.com>
2025-05-04 15:37:09 +0200

    up

--- turbok/ac/ac.js
+++ turbok/ac/ac.js
@@ -1,6 +1,6 @@
 //c compiler
 let ac=x=>{
- let A=[],O=x=>A.push(x)
+ let A=[],O=(x,p)=>A.push(x+(p?" @"+p:""))
  let c=cparse(x)            //cparse.js
 //console.log(c)
  let pos=0,rtyp="i",args=[],locs={},globs={},funcs={},tab={name:"",f:[]},imports={},strings={},top=1
@@ -18,21 +18,21 @@ let ac=x=>{
  let E=x=>{throw new Error("ac:"+pos+": "+x)}
  let glob=(t,s)=>(O(tp(t)+" "+s),(globs[s]=tp(t)))
  let cast=(r,x,t)=>(t=emit(x),(unp(t)!=unp(r))?O(unp(r)+"o"+unp(t)):0,r)
- let bini=(x,y,op,o,t)=>{o=ops[op];
+ let bini=(x,y,op,p,o,t)=>{o=ops[op];
   return(op==":")?tern(x,y)
   :mods.includes(op)?assign(x,{type:"BinaryExpression",left:x,right:y,operator:op.slice(0,-1)})
   :(!o)?E("unknown operator: "+op)
   :"asn"==o?assign(x,y)
-  :(ew(o,"?")||ew(o,"="))?dya(x,y,o):E("unknown operator: "+x)}
- let dya=(x,y,o,c)=>(c=ew(o,"="),x=emt(x),y=emt(y),t=t2(x[0],y[0]),cast(t,{type:"asm",t:x[0],v:x[1]}),(ptr(y[0])&&(!c)?pts(y[0]):0),cast(t,{type:"asm",t:y[0],v:y[1]}),(ptr(x[0])&&(!c)?pts(x[0]):0),O(String(o).slice(0,-1)+t),(c?"i":t))
+  :(ew(o,"?")||ew(o,"="))?dya(x,y,o,p):E("unknown operator: "+x)}
+ let dya=(x,y,o,p,c)=>(c=ew(o,"="),x=emt(x),y=emt(y),t=t2(x[0],y[0]),cast(t,{type:"asm",t:x[0],v:x[1]}),(ptr(y[0])&&(!c)?pts(y[0]):0),cast(t,{type:"asm",t:y[0],v:y[1]}),(ptr(x[0])&&(!c)?pts(x[0]):0),O(String(o).slice(0,-1)+t,p),(c?"i":t))
  let getset=x=>(x in args)?[args[x].t,"get "+args[x].i,"set "+args[x].i]:(x in locs)?[locs[x],"get "+x,"set "+x]:(x in globs)?[globs[x],"glo "+x,"gst "+x]:E("cannot locate variable: "+x)
- let incdec=(x,add,post,t,g,s)=>([t,g,s]=getset(x),O(g),O(t+" 1"),O(add?"adi":"sui"),O(s),O(g), (post?(O(t+" 1"),(O(add?"sui":"adi"))):0),t)
- let suf=(x,o,g,s)=>(x.type!="Identifier")?E("suffix expr: expect identifier"):o=="++"?incdec(x.value,1,1):o=="--"?incdec(x.value,0,1):E("unknown suffix operator: "+o)
+ let incdec=(x,add,post,p,t,g,s)=>([t,g,s]=getset(x),O(g),O(t+" 1"),O(add?"adi":"sui",p),O(s,p),O(g,p),(post?(O(t+" 1",p),(O(add?"sui":"adi",p))):0),t)
+ let suf=(x,o,g,s)=>(x.type!="Identifier")?E("suffix expr: expect identifier"):o=="++"?incdec(x.value,1,1,x.pos):o=="--"?incdec(x.value,0,1,x.pos):E("unknown suffix operator: "+o)
  let pre=(x,o,t,g,s)=>"sizeof"==o?(x=emt(x),O(x[1]+" "+("?4848"[1+"ilef".indexOf(x[1])])),x[1])
   :"-"==o?(t=emit(x),O("ng"+t),t)
   :"!"==o?(t=emit(x),("ij".includes(t)?O("ez"+t):E("! expects integer")))
   :"~"==o?(t=emit(x),("ij".includes(t)?(O("ng"+t),O(t+" 1"),O("su"+t)):E("~ expects integer")))
-  :("++"==o||"--"==o)?(x.type!="Identifier"?E("++/-- prefix expects identifier"):incdec(x.value,"++"==o,0))
+  :("++"==o||"--"==o)?(x.type!="Identifier"?E("++/-- prefix expects identifier"):incdec(x.value,"++"==o,0,x.pos))
   :E("unknown prefix operator: "+o)
  let tern=(x,y,t)=>{if(x.operator!="?")E("ternary: expect ? binary operator")
   t=emit(x.left),O("if "+t),emit(x.right),O("else"),emit(y),O("end");return t}
@@ -49,21 +49,21 @@ let ac=x=>{
   return"v"	  
  }
  //let siz=t=>[0,1,2,4,8,4,8][1+[" chijef"].indexOf(t)]
- let index=(x,y,t,s)=>{if(x.type!="Identifier")E("index: expect identifier on lhs")
-  return((t=look(x.value)),emit(y),pts(t),O("adi"),O("ld"+(t=t.toLowerCase())),t)}
- let look=(x,r)=>("continue"==x||"break"==x)?(O(x),"v")
-  :(x in args)?(O("get "+args[x].i),args[x].t)
-  :(x in locs)?(O("get "+x),locs[x])
-  :(x in globs)?(O("glo "+x),globs[x])
+ let index=(x,y,p,t,s)=>{if(x.type!="Identifier")E("index: expect identifier on lhs")
+  return((t=look(x.value,p)),emit(y),pts(t),O("adi"),O("ld"+(t=t.toLowerCase())),t)}
+ let look=(x,p,r)=>("continue"==x||"break"==x)?(O(x),"v")
+  :(x in args)?(O("get "+args[x].i,p),args[x].t)
+  :(x in locs)?(O("get "+x,p),locs[x])
+  :(x in globs)?(O("glo "+x,p),globs[x])
   :E("lookup: "+x)
  let locl=(s,t)=>(t=tp(t),O(unp(t)+" "+s),locs[s]=t,"v")
- let lite=x=>(O(ew(x,"f")?"e "+x.slice(0,-1):ew(x,"l")?"j "+x.slice(0,-1):x.includes(".")?"f "+x:"i "+x),A[A.length-1][0])
- let slit=x=>(O("i "+((x in strings)?strings[x]:((strings[x]=top),(top+=1+x.length),top-1-x.length))),"C")
+ let lite=(x,p)=>(O(ew(x,"f")?"e "+x.slice(0,-1):ew(x,"l")?"j "+x.slice(0,-1):x.includes(".")?"f "+x:"i "+x,p),A[A.length-1][0])
+ let slit=(x,p)=>(O("i "+((x in strings)?strings[x]:((strings[x]=top),(top+=1+x.length),top-1-x.length)),p),"C")
  let pf=(x,t)=>(t=emt(x),("e"==t[0]?(emit(x),O("ire"),O("jou")):"f"==t[0]?(emit(x),O("jrf")):cast("j",x))) //special case for e|f: dont auto cast but reinterpret to j
- let call=(f,a,t)=>{let fn=funcs[f]
+ let call=(f,a,p,t)=>{let fn=funcs[f];
   if(!(f in funcs))E("call "+f+": func not defined")
   if(a.length!=fn.a.length)E("call "+f+": wrong number of args")	
-  a.forEach((x,i)=>(("printf"==f&&i==1)?pf(x):cast(fn.a[i],x)));O("cal "+f);return fn.r}
+  a.forEach((x,i)=>(("printf"==f&&i==1)?pf(x):cast(fn.a[i],x)));O("cal "+f,p);return fn.r}
  let ical=(x,r,s)=>(x.type=="CallExpression"&&x.base.type=="Identifier"&&x.base.value==tab.name)?
   (s=r+":"+x.arguments.slice(1).map(emit).join(""),emit(x.arguments[0]),O(s),r):0
  let tabl=x=>(
@@ -71,7 +71,7 @@ let ac=x=>{
   (tab.name=x.name,tab.f=x.value.value.map(x=>x.value))
   :0)
  let iff=(c,b,e)=>(cast("i",c),O("if"),emits(b),(e==undefined?0:(O("else"),emits(e))),O("end"))
- let whl=(c,b)=>(O("while"),emit(c),O("do"),emits(b),O("end"),"v")
+ let whl=(c,b,p)=>(O("while",p),emit(c),O("do",b[0].pos),emits(b),O("end"),"v")
  let dowh=(c,b)=>(O("do"),emits(b),O("while"),emit(c),O("end"),"v")
  let ifor=(i,c,s,b)=>(emit(i),O("while"),emit(c),O("do"),emits(b),emit(s),O("end"))
  let drop=x=>(x=="v"||x=="")?0:O("drp")
@@ -80,39 +80,39 @@ let ac=x=>{
  let emits=x=>(x.forEach(emit),"v")
  let emit=x=>{let tx,ty
   return"asm"==x.type?(A.push(...x.v),x.t)
-  :"BinaryExpression"   ==x.type?bini(x.left,x.right,x.operator)
+  :"BinaryExpression"   ==x.type?bini(x.left,x.right,x.operator,x.pos)
   :"SuffixExpression"   ==x.type?suf(x.value,x.operator)
   :"PrefixExpression"   ==x.type?pre(x.value,x.operator)
   :"ExpressionStatement"==x.type?drop(emit(x.expression))
-  :"Literal"            ==x.type?(x.str?slit(x.value):lite(x.value))
-  :"Identifier"         ==x.type?look(x.value)
+  :"Literal"            ==x.type?(x.str?slit(x.value,x.pos):lite(x.value,x.pos))
+  :"Identifier"         ==x.type?look(x.value,x.pos)
   :"VariableDeclaration"==x.type?locl(x.name,x.defType)
   :"CastExpression"     ==x.type?((tx=ical(x.value,unp(tp(x.targetType))))?tx:(cast(tp(x.targetType),x.value)))
-  :"CallExpression"     ==x.type?call(x.base.value,x.arguments)
+  :"CallExpression"     ==x.type?call(x.base.value,x.arguments,x.pos)
   :"IfStatement"        ==x.type?iff(x.condition,x.body,x.else)
-  :"WhileStatement"     ==x.type?whl(x.condition,x.body)
+  :"WhileStatement"     ==x.type?whl(x.condition,x.body,x.pos)
   :"DoWhileStatement"   ==x.type?dowh(x.condition,x.body)
   :"ForStatement"       ==x.type?ifor(x.init,x.condition,x.step,x.body)
-  :"IndexExpression"    ==x.type?index(x.value,x.index)
+  :"IndexExpression"    ==x.type?index(x.value,x.index,x.pos)
   :"ReturnStatement"    ==x.type?(cast(rtyp,x.value),O("ret"),"v")
   :E("unknown ast type:"+x.type)
  }
- let func=(x,h)=>{args={};locs={}
-  let s={r:unp(tp(x.defType)),a:x.arguments.map(x=>unp(tp(x.defType))).join("")}
+ let func=(x,h,p)=>{args={};locs={}
+  let s={r:unp(tp(x.defType)),a:x.arguments.map(x=>unp(tp(x.defType))).join(""),p:p}
   funcs[x.name]=s;imports[x.name]=1
   if(h)return;    imports[x.name]=0
   x.arguments.forEach((x,i)=>{args[x.name]={i:i,t:tp(x.defType)}})
   rtyp=unp(tp(x.defType))
-  O(x.name+" "+s.r+":"+s.a+" export "+x.name)
+  O(x.name+" "+s.r+":"+s.a+" export "+x.name,p)
   x.body.forEach((x,i,b)=>(x.type=="ReturnStatement"&&b.length==1+i)?cast(rtyp,x.value):emit(x))
  }
  c.forEach(x=>{pos=x.pos;
-   "FunctionDeclaration"==x.type?func(x,0)
-  :"FunctionDefinition"==x.type?func(x,1)
+   "FunctionDeclaration"==x.type?func(x,0,x.pos)
+  :"FunctionDefinition"==x.type?func(x,1,x.pos)
   :"GlobalVariableDeclaration"==x.type?(tabl(x)?0:glob(x.defType,x.name))
   :E("unknown toplevel ast type: "+x.type)
  })
- let h=Object.keys(imports).filter(x=>imports[x]).map(x=>x+" "+funcs[x].r+":"+funcs[x].a+" import env "+x)
+ let h=Object.keys(imports).filter(x=>imports[x]).map(x=>x+" "+funcs[x].r+":"+funcs[x].a+" import env "+x+" @"+funcs[x].p)
  let j=x=>(x.length?x.join("\n")+"\n":"")
  return j(h)+j(A)+j(tab.f.map((x,i)=>"tab "+i+" "+x))+text()
 }
---
+++ turbok/examples/libm.c
@@ -0,0 +1,11 @@
+// most of the math functions that js contains can be imported directly.
+double cos(double x);
+int printf(int x,long int y);
+
+int main(int x){
+ printf("cos(0)=%lf\n",cos(0));
+ printf("cos(1)=%lf\n",cos(1));
+ printf("cos(2)=%lf\n",cos(2));
+ printf("cos(3)=%lf\n",cos(3));
+ return 0;
+}
