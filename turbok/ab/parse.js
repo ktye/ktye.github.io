9433cdc65058d5bf4456f4c290d1fb5422ea3172
elmar <ktye@users.noreply.github.com>
2025-05-25 20:31:13 +0200

    up

--- turbok/ab/parse.js
+++ turbok/ab/parse.js
@@ -7,35 +7,42 @@ let token=(x,s,i)=>(s=0,i=ic(x).map(x=>s=T[14*s+C[x]]).map((x,i)=>x>10?-1:i).fil
  [where(x,s).map(x=>x=="\n"?";":x).toReversed(),where(i,s).toReversed()])
 let where=(x,k)=>x.filter((_,i)=>k[i])
 let left="([{",right="}])"
-let op=":+-*%&|<>=~!,^#_$?@/\\"
+let op=":+-*%&|<>=~!,^#_$?@/\\",nm=".-0123456789"
 let parse=x=>{let[tok,pos]=x
- let ipos=0
+console.log("parse",tok)
+ let ipos=0,typs="ijefIJEF"
+ let l=x=>x[x.length-1]
  let perr=x=>{throw new Error("parse:"+ipos+" "+x)}
- let next=_=>(ipos=pos.pop(),tok.pos())
- let peek=_=>tok[len(tok)-1]
+ let type=(x,t)=>x=="-"||op.includes(x[0])?0:(nm.includes(x[0])?(typs.includes(l(x))?l(x):x.includes(".")?"f":"i"):"i")
+ let next=(r,t)=>(r=tok.pop(),r==undefined?0:(t=type(r),r=[r],r.t=t,r.p=pos.pop(),r))
+ let peek=_=>tok[l(tok)]
  let term=(r,n)=>{
-  r=next();if(r==undefined)return r
+  r=next();if(!r)return r
+console.log("r",r)
   if(left.includes(r)){let o=ipos;r=list();if(!right.includes(next()))perr("close "+o);return r}
-  while(left.includes(peek())){
+  while(left.includes(peek())){ console.log("()")
    n=next()//..
   }
   return r}
- let verb=x=>op.includes(x[0])&&(1==x.length||"='".includes(x[1]))
- let expr=x=>{     if(x==undefined)return x
-  let y=term(),r,v;if(y==undefined)return x
-  if(verb(y)&&!(v=verb(x)){
+ let mona=(x,y,i,m)=>((m="~iezi~jezj-ingi-jngj-enge-fngf|eabe|fabf_efle_fflf%esqe%fsqf"),(i=m.indexOf(x+y.t))<0?perr("monadic"):(y.push(m.slice(2+i,5+i)+" @"+x.p),y))
+ let expr=x=>{if(!x)return x
+  let y=term(),r,v=x=>!x.t
+  if(!y)return x
+  if(v(y)&&!(v(x))){ console.log("dyad")
    r=expr(term())
-   if(r==undefined)perr("1+")
+   if(!r)perr("1+")
    return dyad(x,y,r)
   }
- //r=expr(y)
- //if(v)
- //else
+  r=expr(y)
+  if(!(v(x))) console.log("jux")
+  return mona(x,r)
  }
- console.log(x,i)
+ return expr(term())
 }
 
 /*
+
+
 
 if    cnd{..}
 while cnd{..}
