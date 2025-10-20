7d5cf88b8e9f4e3f2510e52cd6d1a9fda834f28a
U-EMEA\csd80117 <ktye@users.noreply.github.com>
2025-10-20 17:43:17 +0200

    up

--- compile/tests/a.go
+++ compile/tests/a.go
@@ -6,5 +6,12 @@ func f(a, b int32) int32 { return a + b }
 
 func main() {
  	var x int32 = 5
-	Exit(f(x,3))
+	if x > 3 {
+	 	if x < 10 {
+			x = f(3, x)
+		} else {
+			x = 4 + x
+		}
+	}
+	Exit(x)
 }
