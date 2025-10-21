91dc11011e5d9dae64d86261eecf99549915310e
U-EMEA\csd80117 <ktye@users.noreply.github.com>
2025-10-21 18:38:58 +0200

    up

--- compile/tests/a.go
+++ compile/tests/a.go
@@ -2,16 +2,10 @@ package main
 
 import . "github.com/ktye/wg/module"
 
-func f(a, b int32) int32 { return a + b }
+//func f(a, b int32) int32 { return a + b }
 
 func main() {
  	var x int32 = 5
-	if x > 3 {
-	 	if x < 10 {
-			x = f(3, x)
-		} else {
-			x = 4 + x
-		}
-	}
+	x = x << 2
 	Exit(x)
 }
