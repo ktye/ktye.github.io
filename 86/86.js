63906e63f1f0893216254807eb20db20d582966e
elmar <ktye@users.noreply.github.com>
2025-10-23 20:23:04 +0200

    up

---
+++ 86/86.js
@@ -0,0 +1,6 @@
+let M,rip
+let loadelf=u=>{let U=new Uint32Array(u.buffer,0,u.buffer.byteLength>>>2),H=new Uint16Array(u.buffer,0,u.buffer.byteLength>>>1),n=H[28],p=U[8]>>>2,i,s=[],m=0
+ for(i=0;i<n;i++){s.push({o:U[p+2],va:U[p+4],fs:U[p+8]});m=Math.max(m,U[p+4]+U[p+10]);p+=14};M=new Uint8Array(m);s.forEach(x=>M.set(u.subarray(x.o,x.o+x.fs),x.va));rip=U[6]}
+
+let show=_=>{let s="";for(let i=0;i<10;i++)s+=M[rip+i].toString(16).padStart("0")+" ";return s}
+
--- 86/elf.js
+++
@@ -1,7 +0,0 @@
-let elf=u=>{let err=s=>{throw new Error(s)},K=new BigUint64Array(u.buffer,0,u.buffer.byteLength>>>3),U=new Uint32Array(u.buffer,0,u.buffer.byteLength>>>2),H=new Uint16Array(u.buffer,0,u.buffer.byteLength>>>1),r={}
- if(U[0]!=1179403647)err("bad magic")  //https://gist.github.com/x0nu11byt3/bcb35c3de461e5fb66173071a2379779
- let e={type:H[8],machine:H[9],version:U[5],entry:K[3],phoff:K[4],shoff:K[5],eflags:U[12],ehsize:H[26],phentsize:H[27],phnum:H[28],shentsize:H[29],shnum:H[30],shstrndx:H[31]};r.e=e
- let m=0,p=Number(e.phoff>>3n),np=e.phnum;r.p=[];for(let i=0;i<e.phnum;i++){r.p[i]={type:U[2*p],flags:U[2*p+1],offset:K[++p],vaddr:K[++p],paddr:K[++p],filesz:K[++p],memsz:K[++p],align:K[++p]};p++;m=Math.max(m,Number(r.p[i].vaddr+r.p[i].memsz))}
- let mem=new Uint8Array(m);r.p.forEach(p=>mem.set(u.subarray(Number(p.offset),Number(p.offset+p.filesz)),Number(p.vaddr)));r.mem=mem
- return r //entry r.mem[Number(r.e.entry)]..
-}
