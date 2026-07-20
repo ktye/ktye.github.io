9fb57f5db89c46ffd8c09779d6d185390208d71f
U-EMEA\csd80117 <ktye@users.noreply.github.com>
2026-07-20 16:58:44 +0200

    up

--- map/fit/fit.js
+++ map/fit/fit.js
@@ -6,7 +6,7 @@ let parse=b=>{let i,j,r={time:[],dist:[],lat:[],lon:[],bpm:[],alt:[],lap:[]},v=n
  let nh=B(),ver=B(),profile=H(),sz=U(),ep=nh+sz,defs=[];U()==1414088238||e(".FIT")
  let p18=f=>{f.forEach(x=>x.i==2?(r.start=U()):x.i==5?(r.sport=B()):x.i==8?(r.secs=U()/1000):x.i==9?(r.meters=0.1*U()):x.i==26?(r.laps=H()):p+=x.s)}//session
  let p19=(f,l)=>{l={};f.forEach(x=>x.i==2?l.start=U():x.i==8?l.secs=U()/1000:x.i==9?l.meters=U()/10:p+=x.s);r.lap.push(l)}//lap
- let p20=f=>{let s=180/2147483648,la=0,lo=0,di=0,bp=0,al=0;f.forEach(x=>x.i==253?ti=U():x.i==0?la=s*I():x.i==1?lo=s*I():x.i==2?al=H()/5-500:x.i==3?bp=B():x.i==5?di=0.01*U():x.i==78?al=U()/5-500:p+=x.s);
+ let p20=f=>{let s=180/2147483648,ti=0,la=0,lo=0,di=0,bp=0,al=0;f.forEach(x=>x.i==253?ti=U():x.i==0?la=s*I():x.i==1?lo=s*I():x.i==2?al=H()/5-500:x.i==3?bp=B():x.i==5?di=0.01*U():x.i==78?al=U()/5-500:p+=x.s);
   r.time.push(ti);r.lat.push(la);r.lon.push(lo);r.dist.push(di);r.bpm.push(bp);r.alt.push(al)}//record
  p=nh;while(p<ep){let h=B(),lt=h&15;
   if(h&0x40){B();B();let m=H(),nf=B(),f=[],sf=0;for(i=0;i<nf;i++){let fi={i:B(),s:B()};B();sf+=fi.s;f[i]=fi};f.s=sf;defs[lt]=[m,f]}
--- map/fit/unfit.c
+++ map/fit/unfit.c
@@ -1,3 +1,21 @@
+#include<stdio.h>
+#include<io.h>
+#include<fcntl.h>
+#define U getchar()
+#define H (U|U<<8)
+#define I (H|H<<16)
+int main(){
+ _setmode(_fileno(stdin),_O_BINARY);
+ int d[64];
+ int h,t,m,n;while(0<=(h=U)){t=h&15;printf("%d %d\n",h&64,t);
+  if(h&64){n=H,H;d[t]=0;n=U;while(n--){U;d[t]+=255&H;}
+  }else{n=d[t];while(n--)U;
+  }
+ }
+}
+
+
+/*
 let
 unfit=b=>{let p=0,d=[],l=[],B=_=>b[p++],h,t,n,m,z,
 s=11930465,H=_=>B()|B()<<8,I=_=>H()|H()<<16;p=B()
@@ -8,3 +26,4 @@ return l}
 
 //invalids: I()==2147483647 lalo==179.99999555759143
 console.log(unfit(Deno.readFileSync(Deno.args[0])))
+*/
