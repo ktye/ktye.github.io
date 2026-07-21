7748826fce9d4bff1858b8410df977fcc1b3d401
U-EMEA\csd80117 <ktye@users.noreply.github.com>
2026-07-21 18:16:45 +0200

    up

--- map/fit/unfit.c
+++ map/fit/unfit.c
@@ -4,26 +4,22 @@
 #define U getchar()
 #define H (U|U<<8)
 #define I (H|H<<16)
+#define F (double)I/11930465
 int main(){
- _setmode(_fileno(stdin),_O_BINARY);
- int d[64];
- int h,t,m,n;while(0<=(h=U)){t=h&15;printf("%d %d\n",h&64,t);
-  if(h&64){n=H,H;d[t]=0;n=U;while(n--){U;d[t]+=255&H;}
-  }else{n=d[t];while(n--)U;
-  }
- }
-}
+_setmode(_fileno(stdin),_O_BINARY);
+int d[32],t,z,n=U;while(--n)U;
+while(0<=(n=U)){t=n&15;
+if(n&64){H;z=20==(n=H)?t:z;d[t]=0;n=U;while(n--){U;d[t]+=U;U;}
+}else{n=d[t];if(t==z){I;printf("%g %g\n",F,F);n-=12;}while(n--)U;}}}
 
-
-/*
-let
-unfit=b=>{let p=0,d=[],l=[],B=_=>b[p++],h,t,n,m,z,
-s=11930465,H=_=>B()|B()<<8,I=_=>H()|H()<<16;p=B()
-while(p<b.length-2){h=B();t=h&15;if(h&64){H()
-if(20==(m=H()))z=t;d[t]=0;n=B();while(n--){p++;d[t]+=255&H()}}
-else{if(t==z)I(),l.push(I()/s,I()/s),p-=12;p+=d[t]}}
-return l}
-
-//invalids: I()==2147483647 lalo==179.99999555759143
-console.log(unfit(Deno.readFileSync(Deno.args[0])))
-*/
+#include<stdio.h>
+#define U getchar()
+#define H (U|U<<8)
+#define F (double)(H|H<<16)/11930465
+int main(){int d[32],t,z,n=U;
+while(--n)U;while(0<=(n=U)){t=n&15;
+if(n&64){H;z=20==(n=H)?t:z;d[t]=0;
+n=U;while(n--){U;d[t]+=U;U;}}
+else{n=d[t];if(t==z){F;
+printf("%g %g\n",F,F);n-=12;}
+while(n--)U;}}}
